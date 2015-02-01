3624b9dbce69c73e5890b2725028c5f75ad83809
Pavel Tarno <paveltarno@gmail.com>
2015-02-01 13:03:16 +0200

    Initial commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,26 @@
+class PostsController < ApplicationController
+
+  def index
+    #byebug
+    1 + 1
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+end
