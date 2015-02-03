57b5b65e55843b32598ba01f4cd4d2962d776401
Pavel Tarno <paveltarno@gmail.com>
2015-02-03 19:01:31 +0200

    Descided to alter the blog to be a multi user platform!

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,23 @@
 Rails.application.routes.draw do
 
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'users/edit'
+
+  get 'users/destroy'
+
+  get 'users/update'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'posts#index'
 
-  resource :posts
+  resources :users
+  resources :posts
 
   resource :session, only: [:create, :destroy, :new]
 
